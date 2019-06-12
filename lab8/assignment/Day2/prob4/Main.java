f65a4c0f93723f5e1473225fc65bea8e50327187
limaoxuan <life14ok@icloud.com>
2019-06-12 15:14:44 -0500

    Add files via upload

--- lab8/assignment/Day2/prob4/Main.java
+++ lab8/assignment/Day2/prob4/Main.java
@@ -7,8 +7,9 @@ import java.util.Comparator;
 public class Main {
     public static void main(String[] args) {
         String[] names = {"Alexis", "Tim", "Kyleen", "KRISTY"};
-//      Arrays.sort(names, (x,y)-> x.compareTo(y));
-        Arrays.sort(names, String::compareTo);
-        System.out.println(Arrays.toString(names));
+//      Arrays.sort(names, (x,y)-> x.compareToIgnoreCase(y));
+        Arrays.sort(names,String::compareToIgnoreCase);
+        Arrays.stream(names).forEach(System.out::println);
+//        System.out.println(Arrays.toString(names));
     }
 }
